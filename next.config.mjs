13fcb31a9688cd8b22e1cdc00d29c1f2933691ff
GitHub-Account-Saad <159918553+SaadTahir1@users.noreply.github.com>
2025-01-12 09:29:58 +0500

    Second Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    images: {
+        domains: ['cdn.sanity.io'], // Add the domain here
+    },
+};
 
-  };
-  
-  export default nextConfig; // Only the default export
-  
+export default nextConfig;
